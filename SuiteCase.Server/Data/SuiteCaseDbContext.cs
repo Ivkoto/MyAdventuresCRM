b04a549a76171e8cc5c38af5fb9a397159184021
Ivaylo Kostov <ikostov87@gmail.com>
2026-06-12 00:27:03 +0300

    remove save changes ovverride

--- SuiteCase.Server/Data/SuiteCaseDbContext.cs
+++ SuiteCase.Server/Data/SuiteCaseDbContext.cs
@@ -13,23 +13,4 @@ public sealed class SuiteCaseDbContext(DbContextOptions<SuiteCaseDbContext> opti
 
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(SuiteCaseDbContext).Assembly);
     }
-
-    public override Task<int> SaveChangesAsync(CancellationToken ct = default)
-    {
-        var now = DateTime.UtcNow;
-
-        foreach (var customer in ChangeTracker.Entries<Customer>())
-        {
-            if (customer.State == EntityState.Added)
-            {
-                customer.Entity.CreatedAt = now;
-            }
-            if (customer.State == EntityState.Modified)
-            {
-                customer.Entity.UpdatedAt = now;
-            }
-        }
-
-        return base.SaveChangesAsync(ct);
-    }
 }
