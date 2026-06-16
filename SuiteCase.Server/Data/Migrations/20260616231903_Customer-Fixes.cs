07d92f0ca3910a5568b7cc15ab44a240cb9f498f
Ivaylo Kostov <ikostov87@gmail.com>
2026-06-17 02:31:10 +0300

    add migration

---
+++ SuiteCase.Server/Data/Migrations/20260616231903_Customer-Fixes.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SuiteCase.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CustomerFixes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(name: "IX_Customers_NationalIdHash", table: "Customers");
+            migrationBuilder.DropIndex(name: "IX_Customers_PassportNumberHash", table: "Customers");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UpdatedAt",
+                table: "Customers",
+                type: "datetimeoffset",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DeletedAt",
+                table: "Customers",
+                type: "datetimeoffset",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "Customers",
+                type: "datetimeoffset",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(name: "IX_Customers_NationalIdHash", table: "Customers");
+            migrationBuilder.DropIndex(name: "IX_Customers_PassportNumberHash", table: "Customers");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Customers",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Customers",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Customers",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset");
+        }
+    }
+}
